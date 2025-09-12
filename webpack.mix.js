b8d01024519aba52ebd49f09ba708f670bdc54e0
MD SULAIMAN <smensulaiman007@gmail.com>
2025-09-12 12:15:47 +0900

    fix: sidebar added

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,6 +1,5 @@
 const mix = require('laravel-mix');
 
-
 /*
  |--------------------------------------------------------------------------
  | Mix Asset Management
@@ -13,23 +12,21 @@ const mix = require('laravel-mix');
  */
 
 const MomentLocalesPlugin = require('moment-locales-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 
 mix.js('resources/src/main.js', 'public').js('resources/src/login.js', 'public')
     .vue();
 
-    mix.webpackConfig({
-        output: {
-          
-            filename:'js/[name].min.js',
-            chunkFilename: 'js/bundle/[name].[hash].js',
-          },
-        plugins: [
-            new MomentLocalesPlugin(),
-            new CleanWebpackPlugin({
-                cleanOnceBeforeBuildPatterns: ['./js/*']
-              }),
-        ]
-    });
+mix.webpackConfig({
+    output: {
+        filename: 'js/[name].min.js',
+        chunkFilename: 'js/bundle/[name].[hash].js',
+    },
+    plugins: [
+        new MomentLocalesPlugin(),
+        new CleanWebpackPlugin({
+            cleanOnceBeforeBuildPatterns: ['./js/*']
+        }),
+    ]
+});
 
